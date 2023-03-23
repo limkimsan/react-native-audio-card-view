19b477cdcac09f4dfd707a13280afaa0a99b909a
Kimsanlim <kimsanlim27@gmail.com>
2023-03-23 16:30:42 +0700

    Delete example folders

--- src/components/AudioCardViewComponent.js
+++ src/components/AudioCardViewComponent.js
@@ -1,28 +1,31 @@
 import React from 'react'
-import {Text, View} from 'react-native'
+import {Text, View, TouchableOpacity} from 'react-native'
 import AudioPlayerButton from 'react-native-audio-player-button';
 import FAIcon from 'react-native-vector-icons/FontAwesome';
 
 const AudioCardViewComponent = () => {
   const [playingUuid, setPlayingUuid] = React.useState(null)
 
+  const renderAudioBtn = () => {
+    return <AudioPlayerButton
+              audio={null}
+              itemUuid='abcdefg'
+              isSpeakerIcon={true}
+              rippled={true}
+              iconPrimaryColor='#0088ff'
+              iconSecondaryColor='#5da297'
+              playingUuid={playingUuid}
+              updatePlayingUuid={(uuid) => setPlayingUuid(uuid)}
+              containerStyle={{marginTop: 16}}
+              customIcon={<FAIcon/>}
+              customIconSet={{play: 'play-circle', pause: 'pause-circle', mute: 'repeat'}}
+            />
+  }
+  
   return (
-    <View>
-      <Text>====== Audio card view ========</Text>
-      <AudioPlayerButton
-        audio={null}
-        itemUuid='abcdefg'
-        isSpeakerIcon={true}
-        rippled={true}
-        iconPrimaryColor='#0088ff'
-        iconSecondaryColor='#5da297'
-        playingUuid={playingUuid}
-        updatePlayingUuid={(uuid) => setPlayingUuid(uuid)}
-        containerStyle={{marginTop: 16}}
-        customIcon={<FAIcon/>}
-        customIconSet={{play: 'play-circle', pause: 'pause-circle', mute: 'repeat'}}
-      />
-    </View>
+    <TouchableOpacity>
+      {renderAudioBtn()}
+    </TouchableOpacity>
   )
 }
 
